a49698f41097963c34fc75d06e6ce728b0e58766
Minh <112998452+minhtche161354@users.noreply.github.com>
2023-11-12 01:23:58 +0700

    Push Lan 2

--- PROJECT_PRN221/StoreSaleClient/MainWindow.xaml.cs
+++ PROJECT_PRN221/StoreSaleClient/MainWindow.xaml.cs
@@ -3,8 +3,10 @@ using StoreSaleClient.Models;
 using StoreSaleClient.NormModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,28 +21,38 @@ using System.Windows.Shapes;
 
 namespace StoreSaleClient
 {
+
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window 
     {
+        private List<OrderModel> listOrder;
+        //Navigation area
+        private void sidebar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            var selected = sidebar.SelectedItem as NavButton;
+
+            navframe.Navigate(selected.Navlink);
+
+        }
+        private string searchString;
         private TcpClient client;
         private List<Product> products;
         public List<Product> Page;
         public static int Index;
+        private readonly int NumOPage = 6;
+        
         public MainWindow()
         {
             products = new List<Product>();
             Index = 0;
             InitializeComponent();
-            int error_counter = 0;
-            if (!ConnectToServer() && error_counter <= 3)
-            {
-                error_counter++;
-                MessageBox.Show("Server is not connected! Retrying...");
-            }
+            Connector();
             loadData();
-
+            DataContext = this;
         }
         public async void loadData()
         {
@@ -51,9 +63,35 @@ namespace StoreSaleClient
                 products = JsonConvert.DeserializeObject<List<Product>>(hold);
                 if (products != null)
                 {
-                    if (products.Skip(Index * 6).Count() > 0)
+                    if(!string.IsNullOrEmpty(searchString)) 
                     {
-                        Page = products.Skip(Index * 6).Take(6).ToList();
+                    products = products.Where(x=>x.ProductName.ToLower().Contains(searchString.ToLower())).ToList();
+                    }
+                    if (products.Skip(Index * NumOPage).Count() > 0)
+                    {
+                        Page = products.Skip(Index * NumOPage).Take(NumOPage).ToList();
+                    }
+                }
+
+                GenerateBlocks(Page);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public async void Search(string query)
+        {
+            try
+            {
+                products.Clear();
+                string hold = await SendRequest("get_all/**/");
+                products = JsonConvert.DeserializeObject<List<Product>>(hold);
+                if (products != null)
+                {
+                    if (products.Skip(Index * NumOPage).Count() > 0)
+                    {
+                        Page = products.Skip(Index * NumOPage).Take(NumOPage).Where(x=>x.ProductName.ToLower().Contains(query)).ToList();
                     }
                 }
 
@@ -74,21 +112,31 @@ namespace StoreSaleClient
         {
             // Set the number of columns in the UniformGrid
             blockUniformGrid.Columns = 3;
-
+            blockUniformGrid.Children.Clear();
             foreach (Product product in page)
             {
                 var block = new Border
                 {
-                    Background = Brushes.LightBlue,
+                    Background = Brushes.LightGray,
                     Margin = new Thickness(10),
-                    Padding = new Thickness(10)
+                    Padding = new Thickness(10),
+                    CornerRadius = new CornerRadius(8),
                 };
 
                 var textBlock = new TextBlock
                 {
                     Text = product.ProductName,
                     Foreground = Brushes.DarkBlue,
-                    FontSize = 16
+                    FontSize = 16,
+                    HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center,
+                };
+                var textBlock2 = new TextBlock
+                {
+                    Text = product.Price.ToString(),
+                    Foreground = Brushes.Red,
+                    FontSize = 12,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
                 };
                 var image = new Image
                 {
@@ -110,6 +158,7 @@ namespace StoreSaleClient
                     Name = "numberO" + product.ProductId,
                     Text = "1",
                     Width = 30,
+                    TextAlignment = TextAlignment.Center,
                     HorizontalAlignment = HorizontalAlignment.Center,
                     VerticalAlignment = VerticalAlignment.Bottom
                 };
@@ -131,20 +180,23 @@ namespace StoreSaleClient
                 };
                 stackPanel.Children.Add(image);
                 stackPanel.Children.Add(textBlock);
+                stackPanel.Children.Add(textBlock2);
                 stackPanel.Children.Add(button);
                 stackPanel.Children.Add(quantityTextBox);
+                
 
 
                 block.Child = stackPanel;
-
+                
                 blockUniformGrid.Children.Add(block);
             }
         }
-        private List<OrderModel> listOrder;
+        
         private void AddToCurrentOrder(int productID, int quant)
         {
             if (listOrder == null)
             {
+                lstOrderView.Items.Clear();
                 listOrder = new List<OrderModel>();
 
             }
@@ -164,7 +216,9 @@ namespace StoreSaleClient
                     });
                 }
             }
-
+                
+                lstOrderView.ItemsSource = null;
+                lstOrderView.ItemsSource = listOrder;
         }
         private bool ConnectToServer()
         {
@@ -218,14 +272,25 @@ namespace StoreSaleClient
             }
         }
 
+        private void Connector()
+        {
+            try
+            {
+                int error_counter = 0;
+                if (!ConnectToServer() && error_counter <= 3)
+                {
+                    error_counter++;
+                    MessageBox.Show("Server is not connected! Retrying...");
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
         private void BtnPrev_Click(object sender, RoutedEventArgs e)
         {
-            int error_counter = 0;
-            if (!ConnectToServer() && error_counter <= 3)
-            {
-                error_counter++;
-                MessageBox.Show("Server is not connected! Retrying...");
-            }
+            Connector();
             blockUniformGrid.Children.Clear();
             if (Index > 0)
                 Index--;
@@ -236,15 +301,24 @@ namespace StoreSaleClient
 
         private void BtnNext_Click(object sender, RoutedEventArgs e)
         {
-            int error_counter = 0;
-            if (!ConnectToServer() && error_counter <= 3)
-            {
-                error_counter++;
-                MessageBox.Show("Server is not connected! Retrying...");
-            }
+            Connector();
             blockUniformGrid.Children.Clear();
-            if((Index+1)*6<=products.Count())
+            if((Index+1)* NumOPage <= products.Count())
             Index++;
+            loadData();
+        }
+
+        private void ButtonSearch_Click(object sender, RoutedEventArgs e)
+        {
+            Connector();
+            if (!string.IsNullOrEmpty(txtSearch.Text))
+            {
+                searchString = txtSearch.Text;
+            }
+            else
+            {
+                searchString = null;
+            }
             loadData();
         }
     }
--- PROJECT_PRN221/StoreServer/Models/Project_PRN221_ServerContext.cs
+++ PROJECT_PRN221/StoreServer/Models/Project_PRN221_ServerContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace StoreServer.Models
 {
@@ -28,8 +29,8 @@ namespace StoreServer.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=(local);database=Project_PRN221_Server;uid=sa;pwd=123;TrustServerCertificate=true");
+                var ConnectionString = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("MyConnection");
+                optionsBuilder.UseSqlServer(ConnectionString);
             }
         }
 
